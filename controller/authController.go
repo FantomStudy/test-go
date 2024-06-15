4452186992f0a0fcee384e8fed4c146f31e61ec2
Daniil <Daniil@DESKTOP-73A4K0T>
2024-06-16 03:51:45 +0500

    bug fix

--- controller/authController.go
+++ controller/authController.go
@@ -26,63 +26,70 @@ func Register(c *fiber.Ctx) error {
 	if err:=c.BodyParser(&data);err!=nil{
 		fmt.Println("Unable to parse body")
 	}
-	//CCheck if password less than 6
-	if len(data["password"].(string))<= 6{
+	//Check if password is less than 6 characters
+	if len(data["password"].(string))<=6{
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message":"Password must be greater than 6 character",
 		})
 	}
+
 	if !validateEmail(strings.TrimSpace(data["email"].(string))){
 		c.Status(400)
 		return c.JSON(fiber.Map{
 			"message":"Invalid Email Address",
 		})
+
 	}
 	//Check if email already exist in database
 	database.DB.Where("email=?", strings.TrimSpace(data["email"].(string))).First(&userData)
 	if userData.Id!=0{
 		c.Status(400)
-		
+		return c.JSON(fiber.Map{
+			"message":"Email already exist",
+		})
+
 	}
 	user:=models.User{
 		FirstName: data["first_name"].(string),
 		LastName: data["last_name"].(string),
 		Phone: data["phone"].(string),
 		Email: strings.TrimSpace(data["email"].(string)),
+
 	}
 	user.SetPassword(data["password"].(string))
 	err:=database.DB.Create(&user)
-	if err != nil{
+	if err != nil {
 		log.Println(err)
 	}
 	c.Status(200)
-	return c.JSON(fiber.Map{
-		"user":user,
-		"message":"Account created successfully",
-	})
+		return c.JSON(fiber.Map{
+			"user":user,
+			"message":"Account created successfullys",
+		})
 }
 
 func Login(c *fiber.Ctx)error  {
 	var data map[string]string
+ 
 	if err:=c.BodyParser(&data);err!=nil{
 		fmt.Println("Unable to parse body")
 	}
 	var user models.User
 	database.DB.Where("email=?", data["email"]).First(&user)
-	if user.Id == 0 {
+	if user.Id ==0{
 		c.Status(404)
 		return c.JSON(fiber.Map{
-			"message":"Email Address doesn't exists, kindly create account",
+			"message":"Email Address doesn't exit, kindly create an account",
 		})
 	}
-	if err:=user.ComparePassword(data["password"]); err != nil{
+	if err:=user.ComparePassword(data["password"]); err !=nil{
 		c.Status(400)
 		return c.JSON(fiber.Map{
-			"message":"Incorrect password",
+			"message":"incorrect password",
 		})
 	}
-	token, err:=util.GenerateJwt(strconv.Itoa(int(user.Id)),)
+	token,err:=util.GenerateJwt(strconv.Itoa(int(user.Id)),)
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
 		return nil
@@ -90,16 +97,17 @@ func Login(c *fiber.Ctx)error  {
 
 	cookie := fiber.Cookie{
 		Name:"jwt",
-		Value: token,
+		Value:token,
 		Expires: time.Now().Add(time.Hour * 24),
 		HTTPOnly: true,
 	}
 	c.Cookie(&cookie)
 	return c.JSON(fiber.Map{
-		"message":"You have successfully login",
+		"message":"you have successfully login",
 		"user":user,
 	})
 }
+
 type Claims struct{
 	jwt.StandardClaims
 }
